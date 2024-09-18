3b36170bc2586900c6aef02676f74ca86501faeb
HimanshuPantwal <himanshupantwal5@gmail.com>
2024-09-18 22:55:06 +0530

    commit

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -11,6 +11,7 @@ const HomePage = () => {
             <HeroSection/>
             <Category/>
             <HomePageProductCard/>
+            
             <Track/>
             <Testimonial/>
         </Layout>
